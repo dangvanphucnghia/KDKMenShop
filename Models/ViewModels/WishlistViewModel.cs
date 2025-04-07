ec8c3d9b7d3ed6d53233e8b12383f184c5e9da2a
Dang Van Phuc Nghia <nghia@gmail.com>
2025-04-07 14:59:11 +0700

    static page

--- Models/ViewModels/WishlistViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using KDKMenShop.Models.Model;
-
-namespace KDKMenShop.Models.ViewModels
-{
-    public class WishlistViewModel
-    {
-        public List<WishlistItemModel> WishlistItems { get; set; }
-    }
-}
